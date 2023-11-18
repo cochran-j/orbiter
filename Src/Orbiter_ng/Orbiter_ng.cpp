5ac7431a84deb747d03ad7a281ef916b31cf675c
Jack Cochran <jec2581@gmail.com>
2023-11-18 09:19:05 -0600

    Compile Orbiter_ng server on Linux

--- Src/Orbiter_ng/Orbiter_ng.cpp
+++ Src/Orbiter_ng/Orbiter_ng.cpp
@@ -1,6 +1,8 @@
 // Copyright (c) Martin Schweiger
 // Licensed under the MIT License
 
+#ifdef _WIN32
+
 #include <windows.h>
 #include <process.h>
 
@@ -9,3 +11,22 @@ INT WINAPI WinMain (HINSTANCE hInstance, HINSTANCE, LPSTR strCmdLine, INT nCmdSh
 	const char *cmd = "Modules\\Server\\Orbiter.exe";
 	return _execl(cmd, cmd, strCmdLine, NULL);
 }
+
+
+#else /* TODO(jec):  Validate support for non-Linux platforms */
+
+#include <string>
+#include <filesystem>
+#include <unistd.h>
+
+int main(int /*argc*/, char* argv[]) {
+
+    auto cmdPath = std::filesystem::path{"Modules"} / "Server" / "Orbiter";
+#ifdef _WIN32
+    cmdPath += ".exe";
+#endif
+
+    return execv(cmdPath.c_str(), argv);
+}
+
+#endif
