ce39f0c5df2b521e3a79c1c078df7fecbbc368c2
Jack Cochran <jec2581@gmail.com>
2023-11-18 09:20:13 -0600

    Compile Orbitersdk module on Linux
    Plumb DLL support for plugins to work with POSIX DLL functions.

--- Src/Orbitersdk/Orbitersdk.cpp
+++ Src/Orbitersdk/Orbitersdk.cpp
@@ -7,32 +7,65 @@
 // ========================================================================
 
 #include <windows.h>
+#include "DllCompat.h"
 #include <fstream>
 #include <stdio.h>
 
+#ifdef _MSC_VER
 #define DLLCLBK extern "C" __declspec(dllexport)
 #define OAPIFUNC __declspec(dllimport)
+#else
+#define DLLCLBK extern "C"
+#define OAPIFUNC
+#endif
 
+OAPIFUNC void InitLib(HINSTANCE);
+typedef void (*DllExitFunc)(HINSTANCE);
+
+static DllExitFunc DLLExit {};
+static HINSTANCE hMod {};
+
+void DLLENTER DllEnter() {
+    static bool need_init = true;
+    if (!need_init) {
+        return;
+    }
+    need_init = false;
+
+    if (!hMod) {
+        hMod = DllGetInstance(reinterpret_cast<void*>(DllEnter));
+    }
+    if (!hMod) {
+        return;
+    }
+
+    InitLib(hMod);
+    DLLExit = (DllExitFunc)GetProcAddress (hMod, "ExitModule");
+    if (!DLLExit) DLLExit = (DllExitFunc)GetProcAddress (hMod, "opcDLLExit");
+}
+
+void DLLEXIT DllExit() {
+    if (DLLExit && hMod) (*DLLExit)(hMod);
+}
+
+#ifdef _WIN32
 BOOL WINAPI DllMain (HINSTANCE hModule,
 					 DWORD ul_reason_for_call,
 					 LPVOID lpReserved)
 {
-	OAPIFUNC void InitLib (HINSTANCE hModule);
-	typedef void (*DLLEXIT)(HINSTANCE);
-	static DLLEXIT DLLExit;
-
 	switch (ul_reason_for_call) {
 	case DLL_PROCESS_ATTACH:
-		InitLib (hModule);
-		DLLExit = (DLLEXIT)GetProcAddress (hModule, "ExitModule");
-		if (!DLLExit) DLLExit = (DLLEXIT)GetProcAddress (hModule, "opcDLLExit");
+        hMod = hModule;
+        DllEnter();
 		break;
 	case DLL_PROCESS_DETACH:
-		if (DLLExit) (*DLLExit)(hModule);
+        DllExit();
 		break;
 	}
 	return TRUE;
 }
+#endif
+
 
 int oapiGetModuleVersion ()
 {
